ef97dc10f1f4d00179c12795c655baf802590ee5
Hatem Ibrahem <48507516+HatemHosam@users.noreply.github.com>
2024-01-28 11:34:53 +0900

    Update Inflated_ConvNext.py

--- models/Inflated_ConvNext.py
+++ models/Inflated_ConvNext.py
@@ -150,7 +150,7 @@ model_urls = {
 
 url = model_urls['convnext_tiny_1k']
 checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
-ConvNeXt_2d(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs).load_state_dict(checkpoint["model"])
+ConvNeXt_2d(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768]).load_state_dict(checkpoint["model"])
 
 #***************************************************
 #****************************************************
