4006d1e4f4f8fe97a4cd05d2c3578f5120799ddf
Hatem Ibrahem <48507516+HatemHosam@users.noreply.github.com>
2024-01-29 09:07:13 +0900

    Update Inflated_ConvNext.py

--- models/Inflated_ConvNext.py
+++ models/Inflated_ConvNext.py
@@ -158,66 +158,89 @@ def convnext_tiny_2d(pretrained=True,in_22k=False, **kwargs):
 #***************************************************
 #****************************************************
 class Block3D(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, Z, H, W)
+    (2) DwConv -> Permute to (N, Z, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
     def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
         super().__init__()
-        # Adjusted dwconv layer to 3D
-        self.dwconv = nn.Conv3d(dim, dim, kernel_size=(7, 7, 7), padding=(3, 3, 3), groups=dim)
-        self.norm = LayerNorm3D(dim, eps=1e-6)
-        self.pwconv1 = nn.Conv3d(dim, 4 * dim, kernel_size=1) 
+        self.dwconv = nn.Conv3d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        #self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.pwconv1 = nn.Conv3d(dim, 4 * dim, kernel_size=1, groups=dim)
         self.act = nn.GELU()
-        self.pwconv2 = nn.Conv3d(4 * dim, dim, kernel_size=1)
+        #self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.pwconv2 = nn.Conv3d(4 * dim, dim, kernel_size=1, groups=dim)
         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
-                                   requires_grad=True) if layer_scale_init_value > 0 else None
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
         input = x
         x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 4, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = x.permute(0, 2, 3, 4, 1) # (N, C, Z, H, W) -> (N, Z, H, W, C)
         x = self.norm(x)
+        x = x .permute(0, 4, 1, 2, 3)
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
+        x = x.permute(0, 2, 3, 4, 1)
         if self.gamma is not None:
             x = self.gamma * x
-        x = x.permute(0, 4, 1, 2, 3) # (N, H, W, C) -> (N, C, H, W)
+        x = x.permute(0, 4, 1, 2, 3) # (N, Z, H, W, C) -> (N, C, Z, H, W)
 
         x = input + self.drop_path(x)
         return x
 
 class ConvNeXt3D(nn.Module):
-    def __init__(self, in_chans=3, num_classes=101, 
+    r""" ConvNeXt3D
+
+    Args:
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
+        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
+        drop_path_rate (float): Stochastic depth rate. Default: 0.
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
+    """
+    def __init__(self, in_chans=3, num_classes=101, #number of classes for UCF-101, Kinetics-700  
                  depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., 
                  layer_scale_init_value=1e-6, head_init_scale=1.,
                  ):
         super().__init__()
 
-        self.downsample_layers = nn.ModuleList() 
+        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
         stem = nn.Sequential(
-            nn.Conv3d(in_chans, dims[0], kernel_size=(4, 4, 4), stride=(4, 4, 4)),
+            nn.Conv3d(in_chans, dims[0], kernel_size=4, stride=4),
             LayerNorm3D(dims[0], eps=1e-6, data_format="channels_first")
         )
         self.downsample_layers.append(stem)
         for i in range(3):
-            # Adjusted downsample layer to 3D with kernel size (2, 2, 2) and stride (2, 2, 2)
             downsample_layer = nn.Sequential(
-                LayerNorm3D(dims[i], eps=1e-6, data_format="channels_first"),
-                nn.Conv3d(dims[i], dims[i+1], kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+                    LayerNorm3D(dims[i], eps=1e-6, data_format="channels_first"),
+                    nn.Conv3d(dims[i], dims[i+1], kernel_size=(2,2,2), stride=(2,2,2), padding = (1,1,1)),
             )
             self.downsample_layers.append(downsample_layer)
 
-        self.stages = nn.ModuleList() 
-        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
         cur = 0
         for i in range(4):
             stage = nn.Sequential(
                 *[Block3D(dim=dims[i], drop_path=dp_rates[cur + j], 
-                           layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
             )
             self.stages.append(stage)
             cur += depths[i]
 
-        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) 
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
         self.head = nn.Linear(dims[-1], num_classes)
 
         self.apply(self._init_weights)
@@ -233,7 +256,7 @@ class ConvNeXt3D(nn.Module):
         for i in range(4):
             x = self.downsample_layers[i](x)
             x = self.stages[i](x)
-        return self.norm(x.mean([-3, -2, -1])) 
+        return self.norm(x.mean([-3, -2, -1])) # global average pooling, (N, C, H, W, Z) -> (N, C)
 
     def forward(self, x):
         x = self.forward_features(x)
@@ -241,6 +264,11 @@ class ConvNeXt3D(nn.Module):
         return x
 
 class LayerNorm3D(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
     def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(normalized_shape))
@@ -260,7 +288,6 @@ class LayerNorm3D(nn.Module):
             x = (x - u) / torch.sqrt(s + self.eps)
             x = self.weight[:, None, None, None] * x + self.bias[:, None, None, None]
             return x
-        return x
 		
 def ConvNext_tiny_inflated(**kwargs):   #copy_weights_2d_to_3d
     """
